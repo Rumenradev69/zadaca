a515f5c31b958b05011dd8bd41a189b97c874842
Rumenradev69 <122432722+Rumenradev69@users.noreply.github.com>
2023-01-12 11:52:30 +0200

    New zadacha
    Change

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+ int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i += 3) 
+            {
+                Console.WriteLine(i);
+                }
